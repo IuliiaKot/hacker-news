55e3b64d5577db24a726dadd23b53cb59e149262
IuliiaKot <kotlenko.julia@gmail.com>
2022-11-18 00:12:19 -0800

    add main requirements ans dark mode

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import HackerNews from './HackerNews';
 import reportWebVitals from './reportWebVitals';
+import store from "./store";
+import { Provider } from 'react-redux'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><HackerNews /></Provider>
   </React.StrictMode>
 );
 
